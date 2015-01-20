a832905162f61b0cba9b8e811467f83eefcb0c6b
Ronald Hong <ronniejhong@gmail.com>
2015-01-19 19:00:56 -0500

    Fix whitespace issues.

--- lib/lending_club/client.rb
+++ lib/lending_club/client.rb
@@ -3,6 +3,7 @@ Dir[File.expand_path('../client/*.rb', __FILE__)].each{|f| require f}
 
 module LendingClub
   class Client < API
+
     def summary
       Summary.new(get("accounts/#{investor_id}/summary"))
     end
--- lib/lending_club/client/summary.rb
+++ lib/lending_club/client/summary.rb
@@ -3,65 +3,65 @@ require 'bigdecimal'
 module LendingClub
   class Summary
 
-  def initialize(data_hash)
-    @raw = data_hash
-  end
+    def initialize(data_hash)
+      @raw = data_hash
+    end
 
-  attr_reader :raw
+    attr_reader :raw
 
-  def investor_id
-    raw['investorId']
-  end
+    def investor_id
+      raw['investorId']
+    end
 
-  # Available cash amount
-  def available_cash
-    BigDecimal.new(raw['availableCash'])
-  end
+    # Available cash amount
+    def available_cash
+      BigDecimal.new(raw['availableCash'])
+    end
 
-  # Account total
-  def account_total
-    BigDecimal.new(raw['accountTotal'])
-  end
+    # Account total
+    def account_total
+      BigDecimal.new(raw['accountTotal'])
+    end
 
-  # Accrued interest amount
-  def accrued_interest
-    BigDecimal.new(raw['accruedInterest'])
-  end
+    # Accrued interest amount
+    def accrued_interest
+      BigDecimal.new(raw['accruedInterest'])
+    end
 
-  # In-Funding balance
-  def infunding_balance
-    BigDecimal.new(raw['infundingBalance'])
-  end
+    # In-Funding balance
+    def infunding_balance
+      BigDecimal.new(raw['infundingBalance'])
+    end
 
-  # Received interest
-  def received_interest
-    BigDecimal.new(raw['receivedInterest'])
-  end
+    # Received interest
+    def received_interest
+      BigDecimal.new(raw['receivedInterest'])
+    end
 
-  # Received principal
-  def received_principal
-    BigDecimal.new(raw['receivedPrincipal'])
-  end
+    # Received principal
+    def received_principal
+      BigDecimal.new(raw['receivedPrincipal'])
+    end
 
-  # Received late fees
-  def received_late_fees
-    BigDecimal.new(raw['receivedLateFees'])
-  end
+    # Received late fees
+    def received_late_fees
+      BigDecimal.new(raw['receivedLateFees'])
+    end
 
-  # Outstanding principal amount
-  def outstanding_principal
-    BigDecimal.new(raw['outstandingPrincipal'])
-  end
+    # Outstanding principal amount
+    def outstanding_principal
+      BigDecimal.new(raw['outstandingPrincipal'])
+    end
 
-  # Total notes
-  def total_notes
-    raw['totalNotes']
-  end
+    # Total notes
+    def total_notes
+      raw['totalNotes']
+    end
 
-  # Total portfolios
-  def total_portfolios
-    raw['totalPortfolios']
-  end
+    # Total portfolios
+    def total_portfolios
+      raw['totalPortfolios']
+    end
 
   end
 end
