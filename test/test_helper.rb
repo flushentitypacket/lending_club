351984d73c0c315262e9c3a37161cf7833b1d4e6
Ronald Hong <ronniejhong@gmail.com>
2015-01-25 12:44:10 -0500

    Add minitest-reporters.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,6 @@
 require 'lending_club'
 require 'minitest/autorun'
+
+require 'minitest/reporters'
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
