efa2ec6d2d3daeb59dcbdc771c7dc4ebb1ed9244
Ronald Hong <ronniejhong@gmail.com>
2015-01-28 21:44:00 -0500

    Add test for parsing loan responses.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,7 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 reporter_options = { color: true }
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
+def fixture(file)
+  File.new(File.expand_path("../fixtures", __FILE__) + '/' + file)
+end
